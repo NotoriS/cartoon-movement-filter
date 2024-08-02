3f2827d05410a7c7e73c3833c08d89c295d2d91a
Evan Cestnick <evan@cestnick.com>
2024-08-02 03:43:23 -0300

    Removed some of the previous code from edge detection

--- cartoon_movement.py
+++ cartoon_movement.py
@@ -2,27 +2,25 @@ import sys
 import os
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 SPEED_LINE_APPEARANCE_DELAY = 0.2
 SPEED_LINE_LIFETIME = 0.5
 TRACKING_POINT_RESELECTION_DELAY = 0.4
 
 BG_SUBTRACTION_MEANS = 5
-MOVING_EDGE_THRESHOLD = 30
 
 # Parameters for corner detection
-feature_params = dict( 
-    maxCorners = 100,
+cd_params = dict( 
+    maxCorners = 200,
     qualityLevel = 0.01,
-    minDistance = 7,
+    minDistance = 10,
     blockSize = 7
 )
 
 # Parameters for Lucas-Kanade optical flow
 lk_params = dict(
     winSize  = (21, 21),
-    maxLevel = 3,
+    maxLevel = 4,
     criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
 )
 
@@ -75,32 +73,20 @@ while cap.isOpened():
         out.write(frame)
         continue
 
-    # Blur the oringal frame in order to acheive better results from edge detection
-    blurred_frame = cv2.GaussianBlur(frame, (3, 3), 1)
-
-    # Detect edges to determine what areas to emit speed lines from
-    edge_frame = cv2.Canny(blurred_frame, 60, 60)
-
-    # Narrow down the detected edges to the ones that are moving
-    moving_edges = np.zeros_like(edge_frame)
-    moving_edges[no_bg_frame > MOVING_EDGE_THRESHOLD] = edge_frame[no_bg_frame > MOVING_EDGE_THRESHOLD]
-
     if (iteration - BG_SUBTRACTION_MEANS - 1) % round(fps * TRACKING_POINT_RESELECTION_DELAY) == 0:
         mask = np.zeros_like(frame)
-        # Reselect lucas-kanade tracking points to account for new on-screen objects
-        # p0 = np.argwhere(moving_edges > 0)
-        # p0 = p0[:, np.newaxis, :].astype(np.float32)
-        p0 = cv2.goodFeaturesToTrack(old_no_bg_frame, mask = None, **feature_params)
+        # Apply corner detection in order to find features to track using the Lucas-Kanade method
+        p0 = cv2.goodFeaturesToTrack(old_no_bg_frame, mask = None, **cd_params)
     else:
         # Continue using previously selected points
         p0 = good_new.reshape(-1, 1, 2)
 
     # Skip to next iteration if there are no good tracking points yet
-    if len(p0) == 0 or iteration == 0:
+    if len(p0) == 0:
         out.write(frame)
         continue
 
-    # Track edges using the lucas-kanade method
+    # Calculate the optical flow the Lucas-Kanade method
     p1, st, _ = cv2.calcOpticalFlowPyrLK(old_no_bg_frame, no_bg_frame, p0, None, **lk_params)
 
     good_new = p1[st == 1]
